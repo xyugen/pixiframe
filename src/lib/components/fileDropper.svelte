9fe86e24f3dd881f6604e51519b5fc4da4a0f81a
Renz Arias <alex.arias.jorym@gmail.com>
2023-08-16 11:08:33 +0800

    feat: create file validation

--- src/lib/components/fileDropper.svelte
+++ src/lib/components/fileDropper.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { droppedFiles } from "$lib/stores";
 
+    const maxFileSize = 5 * 1024 * 1024;
+
     const handleFileDrop = (dragEvent: DragEvent) => {
         dragEvent.preventDefault();
         const files = dragEvent.dataTransfer?.files;
@@ -25,22 +27,24 @@
 
 
 <div
-    class="h-2/5 max-sm:h-2/6 w-full border-5 border-dashed border-neutral rounded-2xl flex flex-col justify-center items-center gap-4 p-4"
+    class="h-2/5 max-sm:h-2/6 w-full border-5 border-dashed border-neutral rounded-2xl flex flex-col justify-center items-center gap-1 p-4"
     on:drop={handleFileDrop}
     on:dragover={(event) => event.preventDefault()}
     role="form"
 >
-    <label for="file-dropper" class="text-xl max-md:text-lg max-sm:text-sm font-semibold">
-        Drag & drop or browse files
+    <label for="file-dropper" class="text-xl max-md:text-lg max-sm:text-sm font-semibold mb-3">
+        Drag & drop or browse files <br />
     </label>
     <input
         type="file"
         id="file-dropper"
         name="files"
         class="file-input file-input-bordered file-input-neutral max-lg:w-11/12 max-md:file-input-sm"
-        accept="image/*,.png,.jpg,.jpeg,.gif"
+        accept="image/*.,.png,.jpg,.jpeg,.gif"
+        size={maxFileSize}
         on:change={handleFileChange}
         multiple
     />
+    <label for="file-dropper" class="text-center text-sm">5MB max</label>
 </div>
 
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -12,12 +12,19 @@ export const actions: Actions = {
 
         const files = formData.getAll("files") as File[];
         for (const file of files) {
+            if (!validateFile(file)) {
+                continue;
+            }
+
+            // Compress file before uploading
             const buffer = await file.arrayBuffer();
             const compressedBuffer = await compressImage(buffer, getFileExtention(file.name));
             
+            // Get file type and mime type
             const fileExtension = getFileExtention(file.name);
             const mimeType = getFileMimeType(fileExtension);
 
+            // Upload file
             const formData = await handleFormData(compressedBuffer, mimeType);
             formData.append('name', file.name);
             const response = await fetch("/api/upload-image", {
@@ -41,4 +48,23 @@ const handleFormData = async (fileBuffer: Buffer, mimeType?: string) => {
     formData.append('file', compressedBlob);
 
     return formData;
+}
+
+const validateFile = async (file: File) => {
+    // Validate file type
+    const allowedExtentions = ['jpeg', 'jpg', 'png', 'gif'];
+    const fileExtention = getFileExtention(file.name);
+    if (!allowedExtentions.includes(fileExtention)) {
+        console.log(`Invalid file type: ${file.name}`);
+        return false;
+    }
+
+    // Validate file size
+    const maxFileSize = 5 * 1024 * 1024; // 5MB
+    if (file.size > maxFileSize) {
+        console.log(`File size exceeds the limit: ${file.name}`);
+        return false;
+    }
+
+    return true;
 }
